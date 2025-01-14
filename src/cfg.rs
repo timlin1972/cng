1c5d7caf70d3b363e57b6496d59c6bf59585f21a
Tim HC Lin (林弘佶) <timhc.lin@moxa.com>
2025-01-14 21:32:01 +0800

    fix warning

--- src/cfg.rs
+++ src/cfg.rs
@@ -2,7 +2,6 @@ use std::fs::{self, File};
 use std::io::Write;
 use std::path::Path;
 
-use fs2::FileExt;
 use serde::{Deserialize, Serialize};
 
 pub const DEF_NAME: &str = "cng_default";
@@ -28,25 +27,25 @@ pub fn init() {
 
         // lock
         let mut file = File::create(CFG_FILE).unwrap();
-        file.lock_exclusive().unwrap();
+        fs2::FileExt::lock_exclusive(&file).unwrap();
 
         file.write_all(file_content.as_bytes()).unwrap();
 
         // unlock
-        file.unlock().unwrap();
+        fs2::FileExt::unlock(&file).unwrap();
     }
 }
 
 pub fn get_cfg() -> Cfg {
     // lock
     let file = File::open(CFG_FILE).unwrap();
-    file.lock_shared().unwrap();
+    fs2::FileExt::lock_shared(&file).unwrap();
 
     let file_content = fs::read_to_string(CFG_FILE).unwrap();
     let cfg: Cfg = serde_json::from_str(&file_content).unwrap();
 
     // unlock
-    file.unlock().unwrap();
+    fs2::FileExt::unlock(&file).unwrap();
 
     cfg
 }
